fce8c4e97f64d2f49e1bd12636db320c786244a4
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-16 13:17:39 -0500

    ⬆️Updates Dependencies

--- src/poshy-wrap-npm.psm1
+++ src/poshy-wrap-npm.psm1
@@ -1,6 +1,7 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
+#Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
 if (-not (Test-Command npm) -and (-not (Get-Variable -Name PWSHRC_FORCE_MODULES_EXPORT_UNSUPPORTED -Scope Global -ValueOnly -ErrorAction SilentlyContinue))) {
